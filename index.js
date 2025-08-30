6945218fb907650ad8ecf4f4de82bfbb77582581
Adbeast <72255037+nawaz9980@users.noreply.github.com>
2025-08-30 20:12:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Step 1: Install Express if you haven't
+// npm install express
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Route: /get
+app.get('/get', (req, res) => {
+  res.send('Hello World from /get!');
+});
+
+// Route: /new
+app.get('/new', (req, res) => {
+  res.send('Hello World from /new!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
